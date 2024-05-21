876b62be29f5515f446949b3828190cb34cb430b
PramodMandati <pramod.mandati@gmail.com>
2024-05-21 22:51:48 +0530

    added Post model with the required fields

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+    is_draft = models.BooleanField(default=True)
